063a79a12278b3250b5421f6d289189589fdf511
Marcel Haazen <mhaazen@live.nl>
2017-11-25 01:04:59 +0100

    Initial commit

---
+++ include/functions.php
@@ -0,0 +1,100 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: mhaaz
+ * Date: 11/24/2017
+ * Time: 11:02 AM
+ */
+use Michelf\Markdown;
+
+/* General Blog Functions */
+
+function get_post_names(){
+
+    static $_cache = array();
+
+    if(empty($_cache)){
+
+        // Get the names of all the
+        // posts (newest first):
+
+        $_cache = array_reverse(glob('posts/*.md'));
+    }
+
+    return $_cache;
+}
+
+function get_posts($page = 1, $perpage = 0){
+
+    if($perpage == 0){
+        $perpage = 10;
+    }
+
+    $posts = get_post_names();
+
+    // Extract a specific page with results
+    $posts = array_slice($posts, ($page-1) * $perpage, $perpage);
+
+    $tmp = array();
+
+    // Create a new instance of the markdown parser
+    $md = new Markdown();
+
+    foreach($posts as $k=>$v){
+
+        $post = new stdClass;
+
+        // Extract the date
+        $arr = explode('_', $v);
+        $post->date = strtotime(str_replace('posts/','',$arr[0]));
+
+        // The post URL
+        $post->url = '/'.date('Y/m', $post->date).'/'.str_replace('.md','',$arr[1]);
+
+        // Get the contents and convert it to HTML
+        $content = $md->defaultTransform(file_get_contents($v));
+
+        // Extract the title and body
+        $arr = explode('</h1>', $content);
+        $post->title = str_replace('<h1>','',$arr[0]);
+        $post->body = $arr[1];
+
+        $tmp[] = $post;
+    }
+
+    return $tmp;
+}
+
+// Find post by year, month and name
+function find_post($year, $month, $name){
+
+    foreach(get_post_names() as $index => $v){
+        if( strpos($v, "$year-$month") !== false && strpos($v, $name.'.md') !== false){
+
+            // Use the get_posts method to return
+            // a properly parsed object
+
+            $arr = get_posts($index+1,1);
+            return $arr[0];
+        }
+    }
+
+    return false;
+}
+
+// Helper function to determine whether
+// to show the pagination buttons
+function has_pagination($page = 1){
+    $total = count(get_post_names());
+
+    return array(
+        'prev'=> $page > 1,
+        'next'=> $total > $page*6
+    );
+}
+
+
+// Turn an array of posts into a JSON
+function generate_json($posts){
+    return json_encode($posts);
+}
---
+++ index.php
@@ -0,0 +1,65 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: mhaaz
+ * Date: 11/24/2017
+ * Time: 10:50 AM
+ */
+
+require 'vendor/autoload.php';
+require 'include/functions.php';
+
+$app = new Slim\App();
+
+// Fetch DI Container
+$container = $app->getContainer();
+
+// Register Twig View helper
+$container['view'] = function ($c) {
+    $view = new \Slim\Views\Twig('include/views', [
+        //'cache' => 'include/cache'
+    ]);
+
+    // Instantiate and add Slim specific extension
+    $basePath = rtrim(str_ireplace('index.php', '', $c['request']->getUri()->getBasePath()), '/');
+    $view->addExtension(new \Slim\Views\TwigExtension($c['router'], $basePath));
+
+    return $view;
+};
+
+//Override the default Not Found Handler
+$container['notFoundHandler'] = function ($c) {
+    return function ($request, $response) use ($c) {
+        return $c['view']->render($response->withStatus(404), '404.html', [
+            "myMagic" => "Let's roll"
+        ]);
+    };
+};
+
+$app->get('/[{page:[0-9]+}]', function ($request, $response, $args){
+    $page = 1;
+    $page = $page ? (int)$page : 1;
+
+    $posts = get_posts($page);
+
+    return $this->view->render($response, 'index.html', [
+        'posts' => $posts
+    ]);
+    //return $response->write(print_r($posts,true));
+});
+
+$app->get('/{year:[0-9]{4}+}/{month:[0-9]{2}+}/{name}', function ($request, $response, $args){
+    $post = find_post($args['year'],$args['month'],$args['name']);
+
+    if(empty($post)){
+        return $this->view->render($response->withStatus(404), "404.html");
+
+    }
+
+    return $this->view->render($response, 'post.html', [
+        'post' => $post
+    ]);
+});
+
+
+$app->run();
